a7aeaf524fb6e9afb68dab04b939bd41ba9295fa
cd155 <github@postmail.app>
2023-02-12 15:23:03 -0500

    display a list of names and image

--- display-app/src/index.js
+++ display-app/src/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import './index.css';
+
+var data = {
+    "dog1":"https://picsum.photos/id/237/200/300", 
+    "dog2":"https://picsum.photos/200/300?grayscale",
+    "dog3":"https://picsum.photos/seed/picsum/200/300"
+}
+
+var keys = Object.keys(data)
 
 class ShopList extends React.Component {
     render() {
+        var grid = [];
+        for (let i = 0; i < keys.length; i++) {
+            grid.push(
+                <Item
+                    key={i}
+                    name={keys[i]}
+                    url={data[keys[i]]}
+                />
+            );
+        }
+
         return (
-        <div className="shoplist">
-            Hello
-        </div>
+        <table className="shoplist">
+            <tbody>
+                <tr> 
+                    <th>Name</th>
+                    <th>Image</th>
+                </tr>
+                {grid}
+            </tbody>
+        </table>
         );
+    }
+}
+
+class Item extends React.Component {
+    render() {
+        return (
+            <tr> 
+                <td>{this.props.name}</td>
+                <td><img src={this.props.url} alt=""></img></td>
+            </tr>
+        )
     }
 }
 
 // ========================================
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<ShopList />);
+root.render(<ShopList 
+                name={"test"}
+            />);
